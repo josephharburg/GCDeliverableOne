65983675399d32b6de53964a1a010bfe90954fe6
Joseph Harburg <josephharburg@gmail.com>
2019-09-06 16:41:41 -0400

    preliminary code

--- GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
+++ GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
@@ -10,6 +10,12 @@ namespace GCDeliverableOneJH
     {
         static void Main(string[] args)
         {
+            double average, smallest, largest, responseOne, responseTwo, responseThree;
+            Console.WriteLine("Welcome to the currency format converter! Please enter 3 dollar amounts that you wish to be converted!");
+            responseOne = Convert.ToDouble(Console.ReadLine());
+            responseTwo = Convert.ToDouble(Console.ReadLine());
+            responseThree = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("{0} {1} {2}",responseOne, responseTwo, responseThree);
         }
     }
 }
