7b4a5ce57459a7934196a4df1bcc14148fac2f93
Joseph Harburg <josephharburg@gmail.com>
2019-09-09 17:27:07 -0400

    Added currency formats for output

--- GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
+++ GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
@@ -10,33 +10,38 @@ namespace GCDeliverableOneJH
     {
         static void Main(string[] args)
         {
-            double responseOne, responseTwo, responseThree;
+            decimal responseOne, responseTwo, responseThree;
             bool correctInput = true;
             while (correctInput == true)
             {
-                Console.WriteLine("Welcome to the currency format converter! Please enter 3 dollar amounts that you wish to be converted!");
+                Console.WriteLine("Welcome! Please enter 3 dollar amounts that you wish to be converted.");
                 try
                 {
-                    responseOne = Convert.ToDouble(Console.ReadLine());
-                    responseTwo = Convert.ToDouble(Console.ReadLine());
-                    responseThree = Convert.ToDouble(Console.ReadLine());
-                    Console.WriteLine("{0} {1} {2}", responseOne, responseTwo, responseThree);
+                    responseOne = Convert.ToDecimal(Console.ReadLine());
+                    responseTwo = Convert.ToDecimal(Console.ReadLine());
+                    responseThree = Convert.ToDecimal(Console.ReadLine());
                     Calculations(responseOne,responseTwo,responseThree);
+                    decimal total = responseOne + responseTwo + responseThree;
+                    Console.WriteLine("The total of the three numbers in different currencies is as follows:" +
+                        "\nUSA: {0}\nSwedish: {1}\nJapanese: {2}\nThai: {3}", total.ToString("C2"), 
+                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("sv-SE")), 
+                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("ja-JP")), 
+                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("th-TH")));
                     correctInput = false;
                 }
                 catch { Console.WriteLine("Im sorry, that is not a valid response, please try again!"); }
             }
             
         }
-        private static void Calculations(double first, double second, double third)
+        private static void Calculations(decimal first, decimal second, decimal third)
         {
-            double[] arrayOne = { first, second, third};
-            double max = arrayOne.Max();
-            double min = arrayOne.Min();
-            double average = arrayOne.Average();
-            Console.WriteLine("Your largest number is " 
-                + max + "\n Your smallest number is " + min + 
-                "\nThe average of the numbers is " + average);
-        }
+            decimal[] arrayOne = { first, second, third};
+            decimal max = arrayOne.Max();
+            decimal min = arrayOne.Min();
+            decimal average = arrayOne.Average();
+              Console.WriteLine("Your largest number is "
+                + max + ".\nYour smallest number is " + min +
+                ".\nThe average of the numbers is " + average);
+             }
     }
 }
