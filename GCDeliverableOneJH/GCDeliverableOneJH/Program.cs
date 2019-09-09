cdeb87a928d4456660e0dd01ee884b01e87815d1
Joseph Harburg <josephharburg@gmail.com>
2019-09-09 17:59:00 -0400

    final product

--- GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
+++ GCDeliverableOneJH/GCDeliverableOneJH/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace GCDeliverableOneJH
 {
@@ -24,24 +25,24 @@ namespace GCDeliverableOneJH
                     decimal total = responseOne + responseTwo + responseThree;
                     Console.WriteLine("The total of the three numbers in different currencies is as follows:" +
                         "\nUSA: {0}\nSwedish: {1}\nJapanese: {2}\nThai: {3}", total.ToString("C2"), 
-                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("sv-SE")), 
-                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("ja-JP")), 
-                        total.ToString("C", System.Globalization.CultureInfo.CreateSpecificCulture("th-TH")));
+                        total.ToString("C", CultureInfo.CreateSpecificCulture("sv-SE")), 
+                        total.ToString("C", CultureInfo.CreateSpecificCulture("ja-JP")), 
+                        total.ToString("C", CultureInfo.CreateSpecificCulture("th-TH")));
                     correctInput = false;
                 }
-                catch { Console.WriteLine("Im sorry, that is not a valid response, please try again!"); }
+                catch {Console.WriteLine("Im sorry, one of the responses was invalid response, please try again!"); }
             }
             
         }
         private static void Calculations(decimal first, decimal second, decimal third)
         {
-            decimal[] arrayOne = { first, second, third};
+            decimal[] arrayOne = {first, second, third};
             decimal max = arrayOne.Max();
             decimal min = arrayOne.Min();
             decimal average = arrayOne.Average();
               Console.WriteLine("Your largest number is "
                 + max + ".\nYour smallest number is " + min +
-                ".\nThe average of the numbers is " + average);
+                ".\nThe average is " + average);
              }
     }
 }
